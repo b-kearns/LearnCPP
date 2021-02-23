88b03defb7cd00b34881c32ed374821536c7b23f
Brandon Kearns <bkearns@ucsc.edu>
2021-02-23 13:27:11 -0800

    Updated Gitignore to ignore executables. Added modulo exercise

---
+++ q5/modulo.cpp
@@ -0,0 +1,34 @@
+//Write a program that asks the user to input an integer, and tells the user whether the number is even or odd. Write a function called isEven() that returns true if an integer passed to it is even, 
+//and false otherwise. Use the modulus operator to test whether the integer parameter is even.
+#include <iostream>
+using namespace std;
+
+int getInput();
+bool isEven(int x);
+
+int main()
+{
+    int x{getInput()};
+    
+    if(isEven(x))
+        cout << x << " is even.\n";
+    else
+        cout << x << " is odd.\n";
+}
+
+int getInput()
+{
+    int x{};
+    cout << "Enter an integer: ";
+    cin >> x;
+
+    return x;
+}
+
+bool isEven(int x)
+{
+    if(x % 2 > 0)
+        return false;
+    else
+        return true;
+}
